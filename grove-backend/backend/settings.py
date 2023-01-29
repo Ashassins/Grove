638a5d8f769c06cb6a68002db2be7fce328bf2b0
Ashassins <ashenassassinmc@gmail.com>
2023-01-29 06:52:19 -0500

    TESTINGS: add jwt frontend endpoints, but with 0 confidence

--- grove-backend/backend/settings.py
+++ grove-backend/backend/settings.py
@@ -55,16 +55,16 @@ REST_FRAMEWORK = {
     )
 }
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
