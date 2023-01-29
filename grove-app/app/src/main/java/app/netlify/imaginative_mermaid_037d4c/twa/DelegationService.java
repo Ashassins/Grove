d090244f99e19a2836689f8a4b6963613262b8b4
avanishsubbiah <avanishsubbiah1@gmail.com>
2023-01-28 20:36:20 -0500

    Added android build

---
+++ grove-app/app/src/main/java/app/netlify/imaginative_mermaid_037d4c/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.imaginative_mermaid_037d4c.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
